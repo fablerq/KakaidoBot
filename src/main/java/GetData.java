64fd8a08a26cc53be416e3f4986aab20109d3f87
fablerq <kenaz1222@gmail,com>
2019-03-01 13:28:50 +0300

    some grammar fixes

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -6,14 +6,14 @@ public class GetData {
         phones.put("11", "q");
         phones.put("222", "w");
         phones.put("333", "r");
-        phones.put("79312345753", "");
+        phones.put("phone", "");
         return phones;
     }
 
     public static void getQuestions() {
-        Bot.questions.put("Зацени своое рабочее место от 1 до 5 попугаев.", "тип 1");
+        Bot.questions.put("Зацени свое рабочее место от 1 до 5 попугаев.", "тип 1");
         Bot.questions.put("Познакомилиь ли вы с большей части команды?", "тип 2");
-        Bot.questions.put("Вас ознакомили с вашими обязанностями", "тип 2");
+        Bot.questions.put("Вас ознакомили с вашими обязанностями?", "тип 2");
         Bot.questions.put("Как дела?", "тип 3");
         Bot.questions.put("Есть ли у вас какие-либо предложения?", "тип 3");
     }
